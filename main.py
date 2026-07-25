e61a391e14e42996026988fffaf1f62937da6824
JCGAMER <jcgamercontatos@gmail.com>
2026-07-25 13:06:59 -0300

    Force redeploy v1.0.3

--- main.py
+++ main.py
@@ -390,7 +390,7 @@ async def lifespan(app: FastAPI):
     yield
 
 
-app = FastAPI(title="Mobilador Server", version="1.0.2-simples", lifespan=lifespan)
+app = FastAPI(title="Mobilador Server", version="1.0.3-force", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
